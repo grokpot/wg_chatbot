17c8ceb5f80b186e9cd87142b6a99f4bbb8f4a00
Tim Luginbühl <tim.luginbuehl@bfh.ch>
2021-10-05 10:28:48 +0200

    current state

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
+import time
+import date
 import datetime
-from flask import Flask
 
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
@@ -14,19 +15,36 @@ logger = logging.getLogger(__name__)
 TELEGRAM_BOT_NAME = 'sennwgbot'
 TELEGRAM_TOKEN = '2067605659:AAGpRA95XQOzSXQtbc0bMMeoP_jiBLOq8d4'
 
+# https://www.bern.ch/themen/abfall/entsorgungskalender/downloads/entsorgungskalender-2021-web-barroierefrei.pdf/download
+PAPER_COLLECTION_DATES = (
+    date.fromisoformat('2021-10-13'),
+    date.fromisoformat('2021-10-27'),
+    date.fromisoformat('2021-11-10'),
+    date.fromisoformat('2021-11-24'),
+    date.fromisoformat('2021-12-08'),
+    date.fromisoformat('2021-12-22')
+)
 
 
-app = Flask('My little Python App') 
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
 
-@app.route('/') 
-def index(): 
-    return 'Hy you! The time is: ' + str(datetime.datetime.now().time())
+  def exit_gracefully(self, *args):
+    self.kill_now = True
 
 
 def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
     update.message.reply_text(update.message.text)
 
+def notify_in_chat():
+    """This method should get called once a day. Depedending on the date
+       it should send a notification to bring out paper/trash
+    """
+    pass
 
 def main_chatbot() -> None:
     """Start the bot."""
@@ -42,12 +60,17 @@ def main_chatbot() -> None:
     # Start the Bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
+    tasked_was_run = False
+    killer = GracefulKiller()
+    while not killer.kill_now:
+        time.sleep(1)
+        now = datetime.datetime.now()
+        if now.hour == 20 and now.minute == 0 and not task_was_run:
+            task_was_run = True
+            notify_in_chat()
+        if now.hour == 20 and now.minute == 1:
+            task_was_run = False
 
 if __name__ == '__main__':
     main_chatbot()
-    #app.run(host='0.0.0.0')
+
